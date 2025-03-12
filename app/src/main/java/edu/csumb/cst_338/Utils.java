06350f72318eb77b4ee8d09a81ac639ca67c012b
Ethan Ayaay <ayaayethan@gmail.com>
2025-03-12 15:39:18 -0700

    created utils

--- app/src/main/java/edu/csumb/cst_338/Utils.java
+++ app/src/main/java/edu/csumb/cst_338/Utils.java
@@ -1,5 +1,14 @@
 package edu.csumb.cst_338;
 
+/**
+ * A utility class for unity conversions of Celsius to Fahrenheit and vise versa
+ */
 public class Utils {
+    public static double ctof(double celsius) {
+        return (celsius * 1.8) + 32;
+    }
 
+    public static double ftoc(double fahrenheit) {
+        return (fahrenheit * 1.8) + 32;
+    }
 }
